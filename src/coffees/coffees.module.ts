68c1b5483985d7fc4585239a31ebeef0952686b3
agungTuanany <agung.tuanany@gmail.com>
2021-04-01 06:36:09 +0700

    feature(section_4_depencies_injection): factory based providers

--- src/coffees/coffees.module.ts
+++ src/coffees/coffees.module.ts
@@ -1,4 +1,4 @@
-import { Module } from "@nestjs/common";
+import { Injectable, Module } from "@nestjs/common";
 import { TypeOrmModule } from "@nestjs/typeorm";
 
 import { CoffeesController } from "./coffees.controller";
@@ -10,23 +10,34 @@ import { Event } from "../events/entities/event.entity";
 // non class based providers
 import { COFFEE_BRANDS } from "./coffees.constants";
 
-// use class based providers
-class ConfigService {}
-class DevelopmentConfigService {}
-class ProductionConfigService {}
+// factory based providers
+@Injectable()
+export class CoffeeBrandsFactory {
+    create() {
+        /* -- do soemthing here --*/
+
+        return ["Salemba brew", "nestcafe"];
+    }
+}
 
 @Module({
     imports: [TypeOrmModule.forFeature([Coffee, Flavor, Event])],
     controllers: [CoffeesController],
     providers: [
         CoffeesService,
+
+        // {
+        //     provide: COFFEE_BRANDS,
+        //     useFactory: () =>  ["Salemba brew", "nestcafe"],
+        // },
+
+        /* -- OR -- */
+
+        CoffeeBrandsFactory,
         {
             provide: COFFEE_BRANDS,
-            useValue: ["Salemba brew", "nestcafe"],
-        },
-        {
-            provide: ConfigService,
-            useClass: process.env.NODE_ENV === "development" ? DevelopmentConfigService : ProductionConfigService,
+            useFactory: (brandsFactory: CoffeeBrandsFactory) => brandsFactory.create(),
+            inject: [CoffeeBrandsFactory],
         },
     ],
     exports: [CoffeesService],
