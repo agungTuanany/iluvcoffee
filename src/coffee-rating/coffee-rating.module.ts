847771a500a517d7a12a99916cabf2f926372d95
agungTuanany <agung.tuanany@gmail.com>
2021-04-02 18:02:55 +0700

    feature(section_4_depencies_injection): create nest - dynamic module

--- src/coffee-rating/coffee-rating.module.ts
+++ src/coffee-rating/coffee-rating.module.ts
@@ -1,9 +1,19 @@
 import { Module } from "@nestjs/common";
 import { CoffeesModule } from "../coffees/coffees.module";
 import { CoffeeRatingService } from "./coffee-rating.service";
+import { DatabaseModule } from "../database/database.module";
 
 @Module({
-    imports: [CoffeesModule],
+    imports: [
+        CoffeesModule,
+        DatabaseModule.register({
+            type: "postgres",
+            host: "localhost",
+            port: 5432,
+            username: "postgres",
+            password: "pass123",
+        }),
+    ],
     providers: [CoffeeRatingService],
 })
 export class CoffeeRatingModule {}
